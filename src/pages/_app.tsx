d79bedb08cbafbe55f793b3e056e830c28f4b02d
James Tennant <jtennant@ucsc.edu>
2023-05-25 13:39:47 -0700

    Remove theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,10 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
-import theme from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CssVarsProvider theme={theme} defaultMode="system">
+    <CssVarsProvider defaultMode="system">
       <CssBaseline />
       <Head>
         <title>James Tennant</title>
