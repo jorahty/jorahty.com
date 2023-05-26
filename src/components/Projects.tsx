5e3b5bdf4be9091f05bc3e0bd8cf6cdf309c2d3b
James Tennant <jtennant@ucsc.edu>
2023-05-26 13:27:41 -0700

    rm unnecessary code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,8 @@
 import { Link, Typography } from "@mui/joy";
 import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import { useColorScheme } from '@mui/joy/styles';
 import { useEffect, useState } from "react";
 
 export default function Projects() {
-  const { mode, systemMode, setMode } = useColorScheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
